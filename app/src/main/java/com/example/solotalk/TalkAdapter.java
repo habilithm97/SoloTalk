9df532209c8ce727c254bd3f4230a2daa110963f
habilithm97 <habinkim97@gmail.com>
2022-06-02 14:26:08 +0900

    채팅 메시지를 길게 눌러 대화상자를 통해 삭제하기

--- app/src/main/java/com/example/solotalk/TalkAdapter.java
+++ app/src/main/java/com/example/solotalk/TalkAdapter.java
@@ -1,5 +1,8 @@
 package com.example.solotalk;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +17,14 @@ public class TalkAdapter extends RecyclerView.Adapter<TalkAdapter.ViewHolder> {
 
     ArrayList<Talk> items;
 
+    Context context;
+
     @NonNull
     @Override
     public TalkAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
         View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.talk_item, viewGroup, false);
+
+        context = viewGroup.getContext();
 
         return new ViewHolder(itemView);
     }
@@ -36,7 +43,7 @@ public class TalkAdapter extends RecyclerView.Adapter<TalkAdapter.ViewHolder> {
         items.add(item);
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         private final TextView tv;
 
@@ -44,7 +51,41 @@ public class TalkAdapter extends RecyclerView.Adapter<TalkAdapter.ViewHolder> {
             super(itemView);
 
             tv = (TextView)itemView.findViewById(R.id.tv);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int position = getAdapterPosition();
+
+                    // 대화상자 생성
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("삭제하기");
+                    builder.setMessage("선택한 메시지를 삭제하시겠습니까? ");
+                    builder.setIcon(R.drawable.delete);
+                    builder.setPositiveButton("삭제하기", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            deleteItem(position);
+                        }
+                    });
+                    builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    builder.show();
+                    return true;
+                }
+            });
         }
+    }
+
+    public void deleteItem(int position) {
+        items.remove(position);
+        notifyItemRemoved(position); // 특정 아이템 한 개를 삭제
+        // 여러 개의 아이템을 변경
+        notifyItemRangeChanged(position, items.size()); // (변경된 첫 번째 아이템의 위치, 변경된 아이템의 갯수)
     }
 
     public TalkAdapter(ArrayList<Talk> dataSet) {
